12b1f3a3ab7dbb3f3132a7b86d2dd67b612e6334
Gilles Peiffer <gilles.peiffer@outlook.com>
2019-07-31 14:01:31 +0200

    Move code from the IJulia notebooks

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
-using MutableArithmetics
 using Test
+using MutableArithmetics
 
-@testset "MutableArithmetics.jl" begin
-    # Write your own tests here.
+using LinearAlgebra, Base.GMP.MPZ
+
+@testset "MutableArithmetics" begin
+    @testset "matrix-vector product" begin
+        @test DummyBigInt[1 2; 3 4]*DummyBigInt[1; 1] == DummyBigInt[3; 7]
+        @test_throws DimensionMismatch DummyBigInt[0 0; 0 0]*DummyBigInt[]
+        @test_throws DimensionMismatch LinearAlgebra.mul!(DummyBigInt[], DummyBigInt[0 0; 0 0], DummyBigInt[0; 0])
+    end
 end
