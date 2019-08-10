1ab163eb0601bcd8d731ef86d9eef21407daa14d
Benoît Legat <benoit.legat@gmail.com>
2019-08-10 19:41:13 +0200

    Refactor similar to #1

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 [![Coveralls](https://coveralls.io/repos/github/Peiffap/MutableArithmetics.jl/badge.svg?branch=master)](https://coveralls.io/github/Peiffap/MutableArithmetics.jl?branch=master)
 [![Build Status](https://api.cirrus-ci.com/github/Peiffap/MutableArithmetics.jl.svg)](https://cirrus-ci.com/github/Peiffap/MutableArithmetics.jl)
 
-```
+```julia
 using BenchmarkTools
 using MutableArithmetics
 const MA = MutableArithmetics
@@ -27,17 +27,20 @@ c2 = big.(c)
 
 
 println("Default performance: ")
-trial = @benchmark MA.mul!($c2, $A2, $b2)
+trial = @benchmark MA.mul_to!($c2, $A2, $b2)
 display(trial)
 
 # Define MutableArithmetics API for numbers
-MA.mutability(::Type{BigInt}) = MA.IsMutable()
+MA.mutability(::Type{BigInt}, ::typeof(MA.zero!)) = MA.IsMutable()
 MA.zero_impl!(x::BigInt) = Base.GMP.MPZ.set_si!(x, 0)
+MA.mutability(::Type{BigInt}, ::typeof(MA.one!)) = MA.IsMutable()
 MA.one_impl!(x::BigInt) = Base.GMP.MPZ.set_si!(x, 1)
-MA.mul_impl!(x::BigInt, a::BigInt, b::BigInt) = Base.GMP.MPZ.mul!(x, a, b)
-MA.add_impl!(x::BigInt, a::BigInt, b::BigInt) = Base.GMP.MPZ.add!(x, a, b)
+MA.mutability(::Type{BigInt}, ::typeof(MA.mul_to!), ::Type{BigInt}, ::Type{BigInt}) = MA.IsMutable()
+MA.mul_to_impl!(x::BigInt, a::BigInt, b::BigInt) = Base.GMP.MPZ.mul!(x, a, b)
+MA.mutability(::Type{BigInt}, ::typeof(MA.add_to!), ::Type{BigInt}, ::Type{BigInt}) = MA.IsMutable()
+MA.add_to_impl!(x::BigInt, a::BigInt, b::BigInt) = Base.GMP.MPZ.add!(x, a, b)
 
 println("MA performance after defininig interface")
-trial2 = @benchmark MA.mul!($c2, $A2, $b2)
+trial2 = @benchmark MA.mul_to!($c2, $A2, $b2)
 display(trial2)
 ```
--- src/MutableArithmetics.jl
+++ src/MutableArithmetics.jl
@@ -8,11 +8,101 @@ module MutableArithmetics
 
 import LinearAlgebra
 
-# Define Tratis
+# These functions suffixed by `_impl!` fail if the first argument cannot be modified to be the result
+
+# Example of mutable types that can implement this API: BigInt, Array, JuMP.AffExpr, MultivariatePolynomials.AbstractPolynomial
+# `..._impl!` functions are similar to `JuMP.add_to_expression`
+# `...!` functions are similar to `JuMP.destructive_add` and `MOI.Utilities.operate!`
+
+"""
+    add_to_impl!(a, b, c)
+
+Write the result of the sum of `b` and `c` to `a`.
+"""
+function add_to_impl! end
+
+"""
+    add_impl!(a, b)
+
+Write the result of the sum of `a` and `b` to `a`.
+"""
+function add_impl! end
+# Fallback
+add_impl!(a, b) = add_to_impl!(a, a, b)
+
+"""
+    mul_to_impl!(a, b, c)
+
+Write the result of the product of `b` and `c` to `a`.
+"""
+function mul_to_impl! end
+
+"""
+    mul_impl!(a, b)
+
+Write the result of the product of `a` and `b` to `a`.
+"""
+function mul_impl! end
+# Fallback
+mul_impl!(a, b) = mul_to_impl!(a, a, b)
+
+"""
+    muladd_to_impl!(a, b, c, d)
+
+Write the result of `muladd(c, d, b)` to `a`.
+"""
+function muladd_to_impl! end
+
+"""
+    muladd_buf_to_impl!(buf, a, b, c, d)
+
+Write the result of `muladd(c, d, b)` to `a`, possibly modifying `buf`.
+"""
+function muladd_buf_to_impl! end
+# Fallback
+function muladd_buf_to_impl!(buf, a, b, c, d)
+    mul_to_impl!(buf, c, d)
+    return add_to_impl!(a, b, buf)
+end
+
+"""
+    muladd_impl!(a, b, c)
+
+Write the result of `muladd(b, c, a)` to `a`.
+"""
+function muladd_impl! end
+# Fallback
+muladd_impl!(a, b, c) = muladd_to_impl!(a, a, b, c)
+
+"""
+    muladd_buf_impl!(buf, a, b, c)
+
+Write the result of `muladd(b, c, a)` to `a`, possibly modifying `buf`.
+"""
+function muladd_buf_impl! end
+# No fallback as it depends on the type of arguments whether we should
+# redirec to `muladd_buf_to_impl!` or `muladd_impl!`
+
+
+"""
+    zero_impl!(a)
+
+Write the result of `zero(a)` to `a`.
+"""
+function zero_impl! end
+
+"""
+    one_impl!(a)
+
+Write the result of `one(a)` to `a`.
+"""
+function one_impl! end
+
+
+# Define Traits
 abstract type MutableTrait end
 struct IsMutable <: MutableTrait end
 struct NotMutable <: MutableTrait end
-
 
 """
     mutability(T::Type, ::typeof(op), args::Type...)::MutableTrait
@@ -30,6 +120,13 @@ mutability(x, op, args...) = mutability(typeof(x), op, typeof.(args)...)
 Return the sum of `b` and `c`, possibly modifying `a`.
 """
 function add_to! end
+function add_to!(a, b, c)
+    add_to!(a, b, c, mutability(a, add_to!, b, c))
+end
+# generic fallbacks
+add_to!(a, b, c, ::NotMutable) = b + c
+add_to!(a, b, c, ::IsMutable) = add_to_impl!(a, b, c)
+
 
 """
     add!(a, b)
@@ -37,89 +134,116 @@ function add_to! end
 Return the sum of `a` and `b`, possibly modifying `a`.
 """
 function add! end
-
-"""
-    add_to_impl!(a, b, c)
-
-Write the result of the sum of `b` and `c` to `a`.
-"""
-function add_to_impl! end
-
-"""
-    add_impl!(a, b)
-
-Write the result of the sum of `a` and `b` to `a`.
-"""
-function add_impl! end
-
-add!(a, b) = add!(a, b, mutability(a, typeof(add!), b, c))
-function add_to!(a, b, c)
-    add_to!(a, b, c, mutability(a, typeof(add!), b, c))
-end
+add!(a, b) = add!(a, b, mutability(a, add!, b))
 # generic fallbacks
 add!(a, b, ::NotMutable) = a + b
-add_to!(a, b, c, ::NotMutable) = b + c
 add!(a, b, ::IsMutable) = add_impl!(a, b)
-add_to!(a, b, c, ::IsMutable) = add_to_impl!(a, b, c)
-
-add_impl!(a, b) = add_to_impl!(a, a, b)
+mutability(T::Type, ::typeof(add!), U::Type) = mutability(T, add_to!, T, U)
 
 """
-    mul!([a::T], b::T, c::T) where T
+    mul_to!(a, b, c)
 
-Inplace multiplication of `b` and `c`, overwriting `a`. If `a` is not provided `c` is overwritten.
+Return the product of `b` and `c`, possibly modifying `a`.
+"""
+function mul_to! end
+function mul_to!(a, b, c)
+    mul_to!(a, b, c, mutability(a, mul_to!, b, c))
+end
+# generic fallbacks
+mul_to!(a, b, c, ::NotMutable) = b * c
+mul_to!(a, b, c, ::IsMutable) = mul_to_impl!(a, b, c)
+
+"""
+    mul!(a, b)
+
+Return the product of `a` and `b`, possibly modifying `a`.
 """
 function mul! end
-function mul_impl! end
-
-mul!(b::T, c::T) where T = mul!(c, b, c)
-mul!(a::T, b::T, c::T) where T = mul!(a, b, c, mutability(T))
-
+mul!(a, b) = mul!(a, b, mutability(a, mul!, b))
 # generic fallbacks
-mul!(a, b, c, ::NotMutable) = b * c
-mul!(a, b, c, ::IsMutable) = mul_impl!(a, b, c)
+mul!(a, b, ::NotMutable) = a * b
+mul!(a, b, ::IsMutable) = mul_impl!(a, b)
+mutability(T::Type, ::typeof(mul!), U::Type) = mutability(T, mul_to!, T, U)
 
 """
-    mul!([u::T], x::T, y::T, z::T, b::T) where T
+    muladd_to!(a, b, c, d)
 
-Inplace version of `muladd(x,y,z)` overwriting `u`. If `a` is not provided `z` is overwritten.
-An intermediate value needs to be written therefore it is necesarsy to provide a buffer `b`.
+Return `muladd(c, d, b)`, possibly modifying `a`.
 """
-muladd!(x::T, y::T, z::T, b::T) where T = muladd!(z, x, y, z, b)
-muladd!(u::T, x::T, y::T, z::T, b::T) where T = muladd!(u, x, y, z, b, mutability(T))
-# fallback
-muladd!(u, x, y, z, buffer, ::NotMutable) = muladd(x, y, z)
-function muladd!(u, x, y, z, b, ::IsMutable)
-    mul_impl!(b, x, y)
-    add_impl!(u, z, b)
-    u
+function muladd_to! end
+function muladd_to!(a, b, c, d)
+    muladd_to!(a, b, c, d, mutability(a, muladd_to!, b, c, d))
+end
+# generic fallbacks
+muladd_to!(a, b, c, d, ::NotMutable) = muladd(c, d, b)
+muladd_to!(a, b, c, d, ::IsMutable) = muladd_to_impl!(a, b, c, d)
+function mutability(S::Type, ::typeof(muladd_to!), T::Type, U::Type, V::Type)
+    return mutability(S, add_to!, T, typeof(zero(U) * zero(V)))
 end
 
-function zero_impl! end
-zero!(x) = zero!(x, mutability(x))
+"""
+    muladd!(a, b, c)
+
+Return `muladd(b, c, a)`, possibly modifying `a`.
+"""
+function muladd! end
+function muladd!(a, b, c)
+    muladd!(a, b, c, mutability(a, muladd!, b, c))
+end
+# generic fallbacks
+muladd!(a, b, c, ::NotMutable) = muladd(b, c, a)
+muladd!(a, b, c, ::IsMutable) = muladd_impl!(a, b, c)
+function mutability(S::Type, ::typeof(muladd!), T::Type, U::Type)
+    return mutability(S, add!, typeof(zero(T) * zero(U)))
+end
+
+
+"""
+    muladd_buf!(buf, a, b, c)
+
+Return `muladd(b, c, a)`, possibly modifying `a` and `buf`.
+"""
+function muladd_buf! end
+function muladd_buf!(buf, a, b, c)
+    muladd_buf!(buf, a, b, c, mutability(a, muladd!, b, c))
+end
+# generic fallbacks
+muladd_buf!(buf, a, b, c, ::NotMutable) = muladd(b, c, a)
+muladd_buf!(buf, a, b, c, ::IsMutable) = muladd_buf_impl!(buf, a, b, c)
+
+"""
+    zero!(a)
+
+Return `zero(a)`, possibly modifying `a`.
+"""
+function zero! end
+zero!(x) = zero!(x, mutability(x, zero!))
 # fallback
 zero!(x, ::NotMutable) = zero(x)
 zero!(x, ::IsMutable) = zero_impl!(x)
 
-function one_impl! end
-one!(x) = one(x, mutability(x))
+"""
+    one!(a)
+
+Return `one(a)`, possibly modifying `a`.
+"""
+function one! end
+one!(x) = one(x, mutability(x, one!))
 # fallback
 one!(x, ::NotMutable) = one(x)
 one!(x, ::IsMutable) = one_impl!(x)
 
-
-"""
-    mul!(C::AbstractVector{T}, A::AbstractVecOrMat{T}, B::AbstractVector{T})
-
-Computes the product between a matrix of `AbstractMutable`s and a vector of `AbstractMutable`s.
-"""
-function mul!(C::AbstractVector{T}, A::AbstractVecOrMat{T}, B::AbstractVector{T}) where T
-    mul!(C, A, B, mutability(T))
+function mutability(::Type{<:Vector}, ::typeof(mul_to!),
+                    ::Type{<:AbstractVecOrMat}, ::Type{<:AbstractVector})
+    return IsMutable() # Assume the element type of the first vector is of correct type which is the case if it is called from `mul`
 end
-function mul!(C::AbstractVector, A::AbstractVecOrMat, B::AbstractVector, ::NotMutable)
-    LinearAlgebra.mul!(C, A, B)
-end
-function mul!(C::AbstractVector{T}, A::AbstractVecOrMat{T}, B::AbstractVector{T}, ::IsMutable) where T
+function mul_to_impl!(C::Vector, A::AbstractVecOrMat, B::AbstractVector)
+    if mutability(eltype(C), muladd!, eltype(A), eltype(B)) isa NotMutable
+        return LinearAlgebra.mul!(C, A, B)
+    end
+    # If `mutability(S, muladd!, T, U)` is `NotMutable`, we might as well redirect to `LinearAlgebra.mul!(C, A, B)`
+    # in which case we can do `muladd_buf!(mul_buffer, A[aoffs + i], b, C[i])` here instead of
+    # `A[aoffs + i] = muladd_buf!(mul_buffer, A[aoffs + i], b, C[i])` which should be faster
     mB = length(B)
     mA, nA = (size(A, 1), size(A, 2)) # lapack_size is not exposed.
     if mB != nA
@@ -136,25 +260,27 @@ function mul!(C::AbstractVector{T}, A::AbstractVecOrMat{T}, B::AbstractVector{T}
     end
 
     # We need a buffer to hold the intermediate multiplication.
-    mul_buffer = zero(T)
+    mul_buffer = zero(zero(eltype(A)) * zero(eltype(B)))
     for k = 1:mB
         aoffs = (k-1)*Astride
         b = B[k]
         for i = 1:mA
-            muladd!(A[aoffs + i], b, C[i], mul_buffer)
+            # `C[i] = muladd_buf!(mul_buffer, C[i], A[aoffs + i], b)`
+            muladd_buf_impl!(mul_buffer, C[i], A[aoffs + i], b)
         end
     end
     end # @inbounds
-    C
+    return C
 end
 
-function mul(A::AbstractVecOrMat{T}, B::AbstractVector{T}) where T
-    C = similar(B, eltype(A), axes(A,1))
+function mul(A::AbstractVecOrMat{T}, B::AbstractVector{S}) where {T, S}
+    TS = Base.promote_op(LinearAlgebra.matprod, T, S)
+    C = similar(B, TS, axes(A,1))
     # C now contains only undefined values, we need to fill this with actual zeros
     for i in eachindex(C)
-        C[i] = zero(eltype(A))
+        @inbounds C[i] = zero(TS)
     end
-    mul!(C, A, B)
+    return mul_to!(C, A, B)
 end
 
 end # module
