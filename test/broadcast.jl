1118eb4e042a349d4bd91b300f069d69482349e9
Benoît Legat <benoit.legat@gmail.com>
2021-05-30 11:18:50 -0400

    Exclude tests for Julia v1.0

--- test/broadcast.jl
+++ test/broadcast.jl
@@ -8,8 +8,12 @@ const MA = MutableArithmetics
     c = [3, 4]
     @test MA.broadcast!(+, a, b) == [4, 5]
     @test a == [4, 5]
-    alloc_test(() -> MA.broadcast!(+, a, b), 0)
-    alloc_test(() -> MA.broadcast!(+, a, c), 0)
+    # Need to have immutable structs that contain references to be allocated on
+    # the stack: https://github.com/JuliaLang/julia/pull/33886
+    if VERSION >= v"1.5"
+        alloc_test(() -> MA.broadcast!(+, a, b), 0)
+        alloc_test(() -> MA.broadcast!(+, a, c), 0)
+    end
 end
 @testset "BigInt" begin
     x = BigInt(1)
@@ -21,8 +25,10 @@ end
     @test a == [4, 5]
     @test x == 4
     @test y == 5
-    # FIXME This should not allocate but I couldn't figure out where these
-    #       240 come from.
-    alloc_test(() -> MA.broadcast!(+, a, b), 240)
-    alloc_test(() -> MA.broadcast!(+, a, c), 0)
+    if VERSION >= v"1.5"
+        # FIXME This should not allocate but I couldn't figure out where these
+        #       240 come from.
+        alloc_test(() -> MA.broadcast!(+, a, b), 240)
+        alloc_test(() -> MA.broadcast!(+, a, c), 0)
+    end
 end
