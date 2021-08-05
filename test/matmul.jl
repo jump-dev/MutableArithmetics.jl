d0bdaa6de0e55ff1830915e3381a82766fedb4a4
Benoît Legat <benoit.legat@gmail.com>
2021-08-05 10:16:45 +0200

    Nice error message in case of unsupported matrix product (#103)
    * Nice error message in case of unsupported matrix product
    
    * Fix test for Julia v1.0
    
    * Fix syntax

--- test/matmul.jl
+++ test/matmul.jl
@@ -51,6 +51,30 @@ end
     @test MA.operate(convert, Int, 1) === 1
 end
 
+const EXPECTED_ERROR = string(
+    "Cannot multiply a `Matrix{NoProdMutable}` with a ",
+    "`Matrix{NoProdMutable}` because the sum of the product of a ",
+    "`NoProdMutable` and a `NoProdMutable` could not be inferred so a ",
+    "`Matrix{Union{}}` allocated to store the output of the ",
+    "multiplication instead of a `Matrix{Int64}`.",
+)
+
+struct NoProdMutable <: MA.AbstractMutable end
+@static if VERSION < v"1.6"
+    # Hack for making the test work on old Julia versions
+    Base.:*(::NoProdMutable, ::NoProdMutable) = error(EXPECTED_ERROR)
+else
+    function MA.promote_operation(::typeof(*), ::Type{NoProdMutable}, ::Type{NoProdMutable})
+        return Int # Dummy result just to test error message
+    end
+end
+
+function unsupported_product()
+    A = [NoProdMutable() for i = 1:2, j = 1:2]
+    err = ErrorException(EXPECTED_ERROR)
+    @test_throws err A * A
+end
+
 @testset "Errors" begin
     @testset "`promote_op` error" begin
         AT = CustomArray{Int,3}
@@ -89,9 +113,12 @@ end
         A = zeros(2)
         B = zeros(2, 1)
         err = DimensionMismatch(
-                                "Cannot sum or substract a matrix of axes `$(axes(B))` into matrix of axes `$(axes(A))`, expected axes `$(axes(B))`.",
+            "Cannot sum or substract a matrix of axes `$(axes(B))` into matrix of axes `$(axes(A))`, expected axes `$(axes(B))`.",
         )
         @test_throws err MA.mutable_operate!(+, A, B)
+    end
+    @testset "unsupported_product" begin
+        unsupported_product()
     end
 end
 
