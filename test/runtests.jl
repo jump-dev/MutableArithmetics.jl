b3d8ea85cab0e61d2e45d88c22593b70a8973b63
Oscar Dowson <odow@users.noreply.github.com>
2021-10-21 09:55:45 +1300

    Fix hygiene of promote_operation (#123)
    * Fix hygiene of promote_operation
    
    * Add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,4 +18,15 @@ end
 include("matmul.jl")
 include("dispatch.jl")
 include("rewrite.jl")
-include("hygiene.jl")
+
+# It is easy to introduce macro scoping issues into MutableArithmetics,
+# particularly ones that rely on `MA` or `MutableArithmetics` being present in
+# the current scope. To work around that, include the "hygiene" script in a
+# clean module with no other scope.
+
+function _include_sandbox(filename)
+    mod = @eval module $(gensym()) end
+    return Base.include(mod, filename)
+end
+
+_include_sandbox("hygiene.jl")
