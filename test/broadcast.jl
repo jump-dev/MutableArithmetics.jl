922a1868f332a1423d21f1fbebf06ac23e056a05
Oscar Dowson <odow@users.noreply.github.com>
2024-11-18 10:09:46 +1300

    Update test/broadcast.jl

--- test/broadcast.jl
+++ test/broadcast.jl
@@ -31,7 +31,7 @@ end
     @test y == 5
     # FIXME This should not allocate but I couldn't figure out where these
     #       allocations come from.
-    alloc_test_le(() -> MA.broadcast!!(+, a, b), 288)
+    alloc_test_le(() -> MA.broadcast!!(+, a, b), 336)
     alloc_test(() -> MA.broadcast!!(+, a, c), 0)
 end
 
